c615c4794e318fc34792846f6a3abdf1ceb65b34
bobbyaxe61 <bobbyaxe61@gmail.com>
2023-06-08 02:25:55 +0100

    Added Exception Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,23 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Exception;
 use Throwable;
+use App\Helpers\DiscordSuite;
+use App\Helpers\Helper;
+use App\Traits\ApiResponderTrait;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponderTrait;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -31,11 +43,43 @@ class Handler extends ExceptionHandler
      *
      * @param  \Throwable  $exception
      * @return void
-     *
-     * @throws \Exception
      */
     public function report(Throwable $exception)
     {
+        // Get an ov config value
+        $api_exception_report = config('ov.api_exception_report', false);
+
+        if ($api_exception_report) {
+
+            // Thrown when processing HTTP requests is unsuccessful
+            if ($exception instanceof HttpException) {
+                $DiscordSuite = DiscordSuite::messenger();
+                $DiscordSuite->setWebhook(config('ov.discord_webhook_url'))->sendWebhook([
+                    'Message' => $exception->getMessage(),
+                    'Route' => request()->url(),
+                    'File' => $exception->getFile(),
+                    'Line' => $exception->getLine(),
+                    'Code' => $exception->getCode(),
+                    'User' => auth() && auth()->user() ? auth()->user()->name : null,
+                    'ip' => Helper::getClientIpAddress(),
+                ]);
+            }
+
+            // Thrown when an exception occurs.
+            if ($exception instanceof Exception) {
+                $DiscordSuite = DiscordSuite::messenger();
+                $DiscordSuite->setWebhook(config('ov.discord_webhook_url'))->sendWebhook([
+                    'Message' => $exception->getMessage(),
+                    'Route' => request()->url(),
+                    'File' => $exception->getFile(),
+                    'Line' => $exception->getLine(),
+                    'Code' => $exception->getCode(),
+                    'User' => auth() && auth()->user() ? auth()->user()->name : null,
+                    'ip' => Helper::getClientIpAddress(),
+                ]);
+            }
+        }
+
         parent::report($exception);
     }
 
@@ -44,12 +88,51 @@ class Handler extends ExceptionHandler
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Throwable  $exception
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Throwable
+     * @return \Illuminate\Http\Response
      */
     public function render($request, Throwable $exception)
     {
+        // Get an ov config value
+        $api_exception_handler = config('ov.api_exception_handler', false);
+
+        if ($request->expectsJson() && $api_exception_handler) {
+
+            // Thrown when an error occurs when a user makes an unauthenticated request
+            if ($exception instanceof AuthenticationException) {
+                return $this->authenticationFailure();
+            }
+
+            // Thrown when a user makes requests that Auth service does not validated
+            if ($exception instanceof AuthorizationException) {
+                return $this->forbiddenAccess();
+            }
+
+            // Thrown when the request fails Laravel FormValidator validation.
+            if ($exception instanceof ValidationException) {
+                return $this->formProcessingFailure($exception->errors(),'Inappropriate input');
+            }
+
+            // Thrown when HTTP Method is incorrect when requesting routing
+            if ($exception instanceof MethodNotAllowedHttpException) {
+                return $this->wrongRequestType($exception->getMessage());
+            }
+
+            // Thrown when the HTTP requested route can not be found
+            if ($exception instanceof NotFoundHttpException) {
+                return $this->notFound();
+            }
+
+            // Thrown when processing HTTP requests is unsuccessful
+            if ($exception instanceof HttpException) {
+                return $this->unavailableService();
+            }
+
+            // Thrown when an exception occurs.
+            if ($exception instanceof Exception) {
+                return $this->internalServerError($exception->getMessage());
+            }
+        }
+
         return parent::render($request, $exception);
     }
 }

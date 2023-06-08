5012bae5ae78796e03242fc4298d03f825a889eb
bobbyaxe61 <bobbyaxe61@gmail.com>
2023-06-08 03:15:54 +0100

    Added User Flow

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,48 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Casts\BaseUrlCast;
+use App\Casts\GenderCast;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+use Laratrust\Traits\LaratrustUserTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
+    use LaratrustUserTrait;
     use Notifiable;
+    use SoftDeletes;
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+      return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+      return [];
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +51,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'phone', 'gender', 'description', 'birth_date', 'birth_year'
     ];
 
     /**
@@ -25,7 +60,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'deleted_at', 'deleted_by'
     ];
 
     /**
@@ -35,5 +70,137 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'birth_date' => 'date:m-d',
+        'birth_year' => 'string',
+        'blocked' => 'boolean',
+        'providers_allowed' => 'array',
+        'providers_disallowed' => 'array',
+        'providers_details' => 'array',
+        'groups' => 'array',
+        'user_details_verified' => 'boolean',
+        'picture' => BaseUrlCast::class,
+        'gender' => GenderCast::class,
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
     ];
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "booting" method of the model.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        self::creating(function ($model) {
+            $model->id = (string) Str::uuid();
+            $model->created_by = auth()->user()? auth()->user()->id : null;
+
+            /**
+             * This is because some mySql version do not allow default on json columns,
+             * Hence these json columns were set to null on migration.
+             * Then on creation are set to a default of '[]' if no value was given.
+             */
+            $model->providers_allowed = $model->providers_allowed ?? [];
+            $model->providers_disallowed = $model->providers_disallowed ?? [];
+            $model->providers_details = $model->providers_details ?? [];
+            $model->groups = $model->groups ?? [];
+        });
+
+        self::updating(function ($model) {
+            $model->updated_by = auth()->user()? auth()->user()->id : null;
+        });
+
+        self::deleting(function ($model) {
+            $model->deleted_by = auth()->user()? auth()->user()->id : null;
+            $model->save();
+        });
+    }
+
+    /**
+     * Assigns a role to a user using existing relationship with roles
+     * 
+     * @param array:App\Models\Role|array:string $role 
+     * @param App\Models\Team|string|null $team
+     * @return bool
+     */
+    public function assignRole($roles, $team = null)
+    {
+        return $this->attachRoles($roles, $team);
+    }
+
+    /**
+     * Retracts a role from a user using existing relationship with roles
+     * 
+     * @param array:App\Models\Role|array:string $role 
+     * @param App\Models\Team|string|null $team
+     * @return bool
+     */
+    public function retractRole($roles, $team = null)
+    {
+        return $this->detachRoles($roles, $team);
+    }
+
+    /**
+     * Establishes if user is associated to given team
+     * 
+     * @param string $team_name
+     * @return bool
+     */
+    public function belongsToTeam($team_name)
+    {
+        return $this->rolesTeams()->where('name',$team_name)->first();
+    }
+
+    /**
+     * Establishes a has many through relationship with roles table through role_user table
+     * @param void|bool $grouped
+     * @return Illuminate\Support\Collection
+     */
+    public function teamsRoles($grouped = true)
+    {
+        $roles = Role::all();
+        return $this->rolesTeams()->get()->map(function ($item) use ($roles) {
+            return collect($item)->merge(['role'=>$roles->firstWhere('id',$item->pivot->role_id)]);
+        })
+        ->when($grouped, function ($query) {
+            return $query->groupBy('name')->map(function ($item) {
+                return [
+                    'id' => $item->first()->get('id'),
+                    'name' => $item->first()->get('name'),
+                    'display_name' => $item->first()->get('display_name'),
+                    'roles' => $item->pluck('role.name')
+                ];
+            });
+        });
+    }
+
+    /**
+     * Retrieve user team through group
+     * 
+     * @param string $team_id
+     * @param string|null $accessor
+     * @return array|string|null
+     */
+    public function group($team_id, $accessor=null)
+    {
+        if (isset($this->groups[$team_id])) {
+
+            if ($accessor) {
+                return $this->groups[$team_id][$accessor] ?? null;
+            }
+
+            return $this->groups[$team_id];
+        }
+
+        return null;
+    }
 }

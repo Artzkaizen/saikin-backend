f7a6a92a690540f68d2bc8149e95285bb39166be
bobbyaxe61 <bobbyaxe61@gmail.com>
2023-06-09 19:08:49 +0100

    Added User Seeders And Factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(CacheTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }

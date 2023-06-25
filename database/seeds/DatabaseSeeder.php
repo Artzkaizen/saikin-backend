a4a6ee2642e98433e18129d20ec6f865892a9296
bobbyaxe61 <bobbyaxe61@gmail.com>
2023-06-25 05:34:36 +0100

    Added Contacts Factory And Seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CacheTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(ContactTableSeeder::class);
     }
 }

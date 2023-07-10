e23380efea3e3ed6faf99371ca21f1f08702e6c0
bobbyaxe61 <bobbyaxe61@gmail.com>
2023-07-10 14:54:19 +0100

    Bug Fix

--- app/Console/Commands/RolesPermissions.php
+++ app/Console/Commands/RolesPermissions.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Role;
+use App\Models\Permission;
 use Illuminate\Console\Command;
 
 class RolesPermissions extends Command
@@ -12,7 +13,7 @@ class RolesPermissions extends Command
      *
      * @var string
      */
-    protected $signature = 'roles:permissions {--detach} {--attach} {--refresh}';
+    protected $signature = 'roles:permissions {--detach} {--attach} {--refresh} {--force}';
 
     /**
      * The console command description.
@@ -38,20 +39,49 @@ class RolesPermissions extends Command
      */
     public function handle()
     {
+        // Take application down
+        $this->call('down');
+
+        // Detach permissions from roles
         if ($this->option('detach') || $this->option('refresh')){
 
             $roles = Role::all();
             $roles->map(function($role){
                 $role->detachPermissions($role->permissions()->get()->pluck('name')->toArray());
             });
+
+            $this->info('Permissions have been detached form roles');
         }
 
+        // Force delete all roles
+        if ($this->option('refresh')) {
+
+            $permissions = Permission::query()->forceDelete();
+
+            $this->info('Permissions have been dropped');
+        }
+
+        // Seed all roles
+        if ($this->option('refresh')) {
+
+            $PermissionTableSeeder = new \PermissionTableSeeder;
+            $PermissionTableSeeder->run();
+
+            $this->info('Permissions have been seeded');
+        }
+
+        // Attach permissions to roles
         if ($this->option('attach') || $this->option('refresh')) {
 
             $PermissionRoleTableSeeder = new \PermissionRoleTableSeeder;
             $PermissionRoleTableSeeder->run();
+
+            $this->info('Permissions have been attached to roles');
         }
 
-        return;
+        // Take application up
+        $this->call('up');
+
+        return ;
     }
 }

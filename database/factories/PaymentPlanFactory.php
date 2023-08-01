25b89a6fdf10669373f7f55b0dfcb96e0cd6c12e
bobbyaxe61 <bobbyaxe61@gmail.com>
2023-08-01 01:54:25 +0100

    Added Payment Plan

---
+++ database/factories/PaymentPlanFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\PaymentPlan;
+use Faker\Generator as Faker;
+
+$factory->define(PaymentPlan::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2023_07_31_212253_create_payment_plans_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_plans', function (Blueprint $table) {
+            // Identifications
+            $table->id();
+
+            // Properties - payment plan
+            $table->string('name');
+            $table->unsignedSmallInteger('level')->nullable();
+            $table->json('payment_plan_benefits')->nullable();
+
+            // Status
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_plans');
+    }
+}

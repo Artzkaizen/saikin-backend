25b24f97aeef9ebac1c90526aebc0ef6c18aac56
bobbyaxe61 <bobbyaxe61@gmail.com>
2023-08-03 22:17:46 +0100

    Comply With Psr-4 Auto Loading

--- app/Http/Requests/PublicPaymentPlanControllerRequests/PublicPaymentPlanPayRequest.php
+++ app/Http/Requests/PublicPaymentPlanControllerRequests/PublicPaymentPlanPayRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\PublicPaymentPlanPayControllerRequests;
+namespace App\Http\Requests\PublicPaymentPlanControllerRequests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class PublicPaymentPlanShowRequest extends FormRequest
+class PublicPaymentPlanPayRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
